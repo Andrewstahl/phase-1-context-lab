c1a38ba02fbd8813061d94d80ab7e9ef07c19786
Andrew Stahl <andrewstahl96@gmail.com>
2022-06-10 10:16:31 -0400

    commit tree b97bc14060d3058ed4846df716a8672eba85dd49

--- index.js
+++ index.js
@@ -112,3 +112,50 @@ function findEmployeeByFirstName(srcArray, firstName) {
 
   return undefined;
 }
+
+// Copied from the index text
+// const csvDataEmployees = [
+//   ["Thor", "Odinsson", "Electrical Engineer", 45],
+//   ["Loki", "Laufeysson-Odinsson", "HR Representative", 35],
+//   ["Natalia", "Romanov", "CEO", 150],
+//   ["Darcey", "Lewis", "Intern", 15],
+//   ["Jarvis", "Stark", "CIO", 125],
+//   ["Anthony", "Stark", "Angel Investor", 300]
+// ]
+
+// const csvTimesIn = [
+//   ["Thor", ["2018-01-01 0800", "2018-01-02 0800", "2018-01-03 0800"]],
+//   ["Loki", ["2018-01-01 0700", "2018-01-02 0700", "2018-01-03 0600"]],
+//   ["Natalia", ["2018-01-03 1700", "2018-01-05 1800", "2018-01-03 1300"]],
+//   ["Darcey", ["2018-01-01 0700", "2018-01-02 0800", "2018-01-03 0800"]],
+//   ["Jarvis", ["2018-01-01 0500", "2018-01-02 0500", "2018-01-03 0500"]],
+//   ["Anthony", ["2018-01-01 1400", "2018-01-02 1400", "2018-01-03 1400"]]
+// ]
+
+// const csvTimesOut = [
+//   ["Thor", ["2018-01-01 1600", "2018-01-02 1800", "2018-01-03 1800"]],
+//   ["Loki", ["2018-01-01 1700", "2018-01-02 1800", "2018-01-03 1800"]],
+//   ["Natalia", ["2018-01-03 2300", "2018-01-05 2300", "2018-01-03 2300"]],
+//   ["Darcey", ["2018-01-01 1300", "2018-01-02 1300", "2018-01-03 1300"]],
+//   ["Jarvis", ["2018-01-01 1800", "2018-01-02 1800", "2018-01-03 1800"]],
+//   ["Anthony", ["2018-01-01 1600", "2018-01-02 1600", "2018-01-03 1600"]]
+// ]
+
+// let employeeRecords = createEmployeeRecords(csvDataEmployees)
+// employeeRecords.forEach(function (rec) {
+//   let timesInRecordRow = csvTimesIn.find(function (row) {
+//     return rec.firstName === row[0]
+//   })
+
+//   let timesOutRecordRow = csvTimesOut.find(function (row) {
+//     return rec.firstName === row[0]
+//   })
+
+//   timesInRecordRow[1].forEach(function(timeInStamp){
+//     createTimeInEvent.call(rec, timeInStamp)
+//   })
+
+//   timesOutRecordRow[1].forEach(function(timeOutStamp){
+//     createTimeOutEvent.call(rec, timeOutStamp)
+//   })
+// })
